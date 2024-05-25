1ddb1561c47a4f0e15935bbe8677cb9f43ed1ef2
Bryan Pazmino <bryanpazmi1977@gmail.com>
2024-05-25 13:16:49 -0500

    Se modificaron algunas clases

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    @InjectRepository(User)
+    constructor(private readonly userRepository: Repository<User>){
+        
+    }
+
+}
